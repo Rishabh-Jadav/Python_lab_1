9fab27aa30934a4965503320a42fafe20c2575aa
Rishabh-Jadav <rishabh2031@tnraocollege.org>
2024-07-14 10:35:53 +0530

    employee.py

--- employee.py
+++ employee.py
@@ -2,13 +2,13 @@ EMPNAME = ['Dom', 'Brian', 'Tej']
 print("Initial list of employee names:", EMPNAME)
 
 EMPNAME.append('Laty')
-print("After appending 'David':", EMPNAME)
+print("After appending 'Laty':", EMPNAME)
 
 EMPNAME.remove('Brian')
-print("After removing 'Bob':", EMPNAME)
+print("After removing 'Brian':", EMPNAME)
 
 EMPNAME.insert(1, 'Rom')
-print("After inserting 'Eve' at index 1:", EMPNAME)
+print("After inserting 'Rom' at index 1:", EMPNAME)
 
 # Remove an element by index using pop()
 removed_employee = EMPNAME.pop(2)
